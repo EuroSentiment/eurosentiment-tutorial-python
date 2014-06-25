0ce8ec62f5a1bc268748c3035da67c74b294f056
Mario Muñoz <mariomgal@gmail.com>
2014-06-25 15:18:47 +0200

    Token removed from example.

---
+++ conf/configuration.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#
+#  The original code are licensed under the GNU Lesser General Public License.
+
+TOKEN = 'INSERT_YOUR_TOKEN_HERE'
+
+LANG_DETECTION_URL = 'http://54.187.254.3/services/language_detector'
+RESOURCES_URL = 'http://54.201.101.125/sparql/'
